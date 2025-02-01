93e3896c156065a51ac4c7a93e9c2c8360704520
privindolleth <privindolleth3@gmail.com>
2025-02-01 20:47:37 +0530

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.privin.mm"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 1
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
