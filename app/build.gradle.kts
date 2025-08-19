68247d955f7af34a438c1410fe4da7979c86002d
Privin Dolleth Reyner P <privin@privin.dev>
2025-08-19 17:24:58 +0530

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,12 +16,12 @@ val secretKeyProperties by lazy {
 
 android {
     namespace = "com.privin.mm"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.privin.mm"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
